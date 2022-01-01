0133810f7af7db25bef112e74891fe15384ec8f4
F1lh0sQu3imada <antoniopintotrabalho@gmail.com>
2022-01-01 14:45:59 +0000

    Added breeds information method /api/breeds

--- server.js
+++ server.js
@@ -1,63 +1,73 @@
 const express = require("express");
-const bodyParser = require("express");
 const fs = require("fs");
 const PORT = process.env.PORT || 8080;
 
-let {incrementCounter} = require("./counter");
+let { incrementCounter } = require("./counter");
 let updtDt = require("./updatedata");
 let app = express();
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 let breeds, animaltypes = null;
 
-while(!breeds || !animaltypes){
-    try{
-        breeds = JSON.parse(fs.readFileSync("data/breeds.json","utf-8"));
-        animaltypes = JSON.parse(fs.readFileSync("data/animaltypes.json","utf-8"));
-    }catch(err){
+while (!breeds || !animaltypes) {
+    try {
+        breeds = JSON.parse(fs.readFileSync("data/breeds.json", "utf-8"));
+        animaltypes = JSON.parse(fs.readFileSync("data/animaltypes.json", "utf-8"));
+    } catch (err) {
         updtDt.updateData();
     }
 }
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.use("/api", (req, res, next)=>{
+app.use("/api", (req, res, next) => {
     incrementCounter();
     next();
 })
 
-app.get("/api/types/:id?", (req, res) =>{
+app.get("/api/types/:id?", (req, res) => {
     let response = [];
-    let begin_date = Date.now(); 
+    let begin_date = Date.now();
     animaltypes.collection.forEach(element => {
-        if(!req.params.id && !req.body.type){
-            console.log(1);
-            response.push({"id": element.resource.id, "type": element.resource.slug})
-        }else if(!req.params.id ^ !req.body.type){
-            if(!!req.params.id && req.params.id == element.resource.id){
-                console.log(2);
-                response.push({"id": element.resource.id, "type": element.resource.slug})
+        if (!req.params.id && !req.body.type) {
+            response.push({ "id": element.resource.id, "type": element.resource.slug })
+        } else if (!req.params.id ^ !req.body.type) {
+            if (!!req.params.id && req.params.id == element.resource.id) {
+                response.push({ "id": element.resource.id, "type": element.resource.slug })
             }
-    
-            if(!!req.body.type && req.body.type == element.resource.slug){
-                console.log(3);
-                response.push({"id": element.resource.id, "type": element.resource.slug})
+            if (!!req.body.type && req.body.type == element.resource.slug) {
+                response.push({ "id": element.resource.id, "type": element.resource.slug })
             }
-        }  
+        }
+    });
+    res.send(prettyJSON(response, Date.now() - begin_date));
+});
+
+app.get("/api/breeds/:id?", (req, res) => {
+    let response = [];
+    let begin_date = Date.now();
+    breeds.collection.forEach(element => {
+        if (!!req.params.id ^ !!req.body.breed ^ !!req.body.type) { //if only one is not undefined or not null
+            if (element.resource.lib == req.body.breed) {
+                response.push({ "id": element.resource.id, "breed": element.resource.lib, "type": element.resource.type.slug });
+            } else if (element.resource.type.slug == req.body.type) {
+                response.push({ "id": element.resource.id, "breed": element.resource.lib, "type": element.resource.type.slug });
+            } else if (element.resource.id == req.params.id) {
+                response.push({ "id": element.resource.id, "breed": element.resource.lib, "type": element.resource.type.slug });
+            }
+        } else { //if id is not undefined or null
+            response.push({ "id": element.resource.id, "breed": element.resource.lib, "type": element.resource.type.slug });
+        }
     });
 
     res.send(prettyJSON(response, Date.now() - begin_date));
 });
 
-app.get("/api/breeds", (req, res) =>{
-    res.send("ola");
-});
-
-function prettyJSON(response, responseTime){
+function prettyJSON(response, responseTime) {
     return {
         "response": response,
         "res_size": response.length,
